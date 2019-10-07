2a0a9a5b5645b15e0a7c24f0f6a772c7535966d4
Dan Ledger <dan@runjumplabs.com>
2019-10-06 20:36:49 -0400

    updated attempt

--- make_release.sh
+++ make_release.sh
@@ -6,10 +6,11 @@ cp -R ~/Documents/Arduino/hardware/dreammaker_fx/samd/* temp/dreammaker_fx_1.1.0
 mkdir temp/dreammaker_fx_1.1.0/libraries/dmfx
 cp -R ~/Documents/Arduino/libraries/dmfx-arduino/* temp/dreammaker_fx_1.1.0/libraries/dmfx/
 
-mkdir temp/dreammaker_fx_1.1.0/examples
-mkdir temp/dreammaker_fx_1.1.0/examples/dmfx_firmware_updater
-cp -R ~/Github/Dreammaker_FX/dmfx-firmware-updater/dmfx-firmware-updater/* temp/dreammaker_fx_1.1.0/examples/dmfx_firmware_updater/
+#mkdir temp/dreammaker_fx_1.1.0/examples
+#mkdir temp/dreammaker_fx_1.1.0/examples/dmfx_firmware_updater
+#cp -R ~/Github/Dreammaker_FX/dmfx-firmware-updater/dmfx-firmware-updater/* temp/dreammaker_fx_1.1.0/examples/dmfx_firmware_updater/
 
+mkdir temp/dreammaker_fx_1.1.0/bootloaders
 
 rm arduino-board-index/boards/dreammaker_fx-samd-1.1.0.tar.bz2
 cd temp/dreammaker_fx_1.1.0/
