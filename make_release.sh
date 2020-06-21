78d5e43e4f82d3ce1d24b78e5a3ba5447b617f5f
Dan Ledger <dan@runjumplabs.com>
2020-06-21 19:09:59 -0400

    1.6.5

--- make_release.sh
+++ make_release.sh
@@ -1,29 +1,29 @@
 export COPY_EXTENDED_ATTRIBUTES_DISABLE=true
 
 mkdir temp
-mkdir temp/dreammaker_fx_1.6.4
+mkdir temp/dreammaker_fx_1.6.5
 
-cp -R ~/Documents/Arduino/hardware/dreammaker_fx/samd/* temp/dreammaker_fx_1.6.4/
+cp -R ~/Documents/Arduino/hardware/dreammaker_fx/samd/* temp/dreammaker_fx_1.6.5/
 
-mkdir temp/dreammaker_fx_1.6.4/libraries/dmfx
-cp -R ~/Documents/Arduino/libraries/dmfx-arduino/* temp/dreammaker_fx_1.6.4/libraries/dmfx/
-rm -R temp/dreammaker_fx_1.6.4/libraries/dmfx/.git/
+mkdir temp/dreammaker_fx_1.6.5/libraries/dmfx
+cp -R ~/Documents/Arduino/libraries/dmfx-arduino/* temp/dreammaker_fx_1.6.5/libraries/dmfx/
+rm -R temp/dreammaker_fx_1.6.5/libraries/dmfx/.git/
 
 #mkdir temp/dreammaker_fx_1.1.0/examples
 #mkdir temp/dreammaker_fx_1.1.0/examples/dmfx_firmware_updater
 #cp -R ~/Github/Dreammaker_FX/dmfx-firmware-updater/dmfx-firmware-updater/* temp/dreammaker_fx_1.1.0/examples/dmfx_firmware_updater/
 
-mkdir temp/dreammaker_fx_1.6.4/bootloaders
+mkdir temp/dreammaker_fx_1.6.5/bootloaders
 
-rm arduino-board-index/boards/dreammaker_fx-samd-1.6.4.tar.bz2
+rm arduino-board-index/boards/dreammaker_fx-samd-1.6.5.tar.bz2
 cd temp/
-tar --exclude=".DS_Store" --disable-copyfile -cvjSf ../arduino-board-index/boards/dreammaker_fx-samd-1.6.4.tar.bz2 *
+tar --exclude=".DS_Store" --disable-copyfile -cvjSf ../arduino-board-index/boards/dreammaker_fx-samd-1.6.5.tar.bz2 *
 #tar -czvf ../../arduino-board-index/boards/dreammaker_fx-samd-1.1.0.tar.gz  *
 cd ..
 echo "SHA256:"
-shasum -a 256 arduino-board-index/boards/dreammaker_fx-samd-1.6.4.tar.bz2
+shasum -a 256 arduino-board-index/boards/dreammaker_fx-samd-1.6.5.tar.bz2
 
 echo "SIZE:"
-stat -x arduino-board-index/boards/dreammaker_fx-samd-1.6.4.tar.bz2
+stat -x arduino-board-index/boards/dreammaker_fx-samd-1.6.5.tar.bz2
 
 rm -R temp
